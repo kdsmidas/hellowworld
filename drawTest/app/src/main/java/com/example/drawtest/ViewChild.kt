ec76b7419020102b7be7e36116094fccc38901ea
kdsmidas <kdsmidas@gmail.com>
2020-03-26 07:14:14 +0900

    2nd

---
+++ drawTest/app/src/main/java/com/example/drawtest/ViewChild.kt
@@ -0,0 +1,12 @@
+package com.example.drawtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewChild : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_child)
+    }
+}
